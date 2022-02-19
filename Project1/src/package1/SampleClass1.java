00fee69d846ccabb2847e45461f0728af9b34653
Trivikram-Sharma <vickygosukonda@gmail.com>
2022-02-19 23:51:20 +0530

    Added Project1

---
+++ Project1/src/package1/SampleClass1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class SampleClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++) {
+			System.out.println("Hello");
+		}
+	}
+
+}
